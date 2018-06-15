c192fa05bb471498dd9004e47e4eef49b3446cf1
dandgerson <dandgerson@gmail.com>
2018-06-15 11:12:59 +0400

    webpack-dev-server

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,17 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Webpack demo',
+    }),
+  ],
+
+  devServer: {
+    stats: 'errors-only',
+    host: process.env.HOST,
+    port: process.env.PORT,
+    open: false,
+    overlay: false,
+  },
+};
