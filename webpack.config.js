a7cce8b395f620b5df7a08e543304bc71fc42af8
dandgerson <dandgerson@gmail.com>
2018-06-25 17:13:49 +0300

    HMR setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,10 @@
 'use strict';
 
+const path = require('path');
+
+const webpack = require('webpack');
 const merge = require('webpack-merge');
+
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const parts = require('./webpack.parts');
@@ -21,11 +25,24 @@ const commonConfig = merge([
 const productionConfig = merge([]);
 
 const developmentConfig = merge([
+  {
+    devtool: 'source-map',
+  },
+
   parts.devServer({
     // Customize host/port here if nedeed
     host: process.env.HOST,
     port: process.env.PORT,
   }),
+
+  {
+    plugins: [
+      new webpack.WatchIgnorePlugin([
+        path.join(__dirname, 'node_modules')
+      ]),
+      new webpack.HotModuleReplacementPlugin(),
+    ],
+  },
 ]);
 
 module.exports = mode => {
--- webpack.parts.js
+++ webpack.parts.js
@@ -3,10 +3,15 @@
 exports.devServer = ({ host, port } = {}) => ({
   devServer: {
     // stats: 'errors-only',
+    
     host, // Defaults to 'localhost'
     port, // Defaults to 8080
+
     open: false,
     overlay: true,
+    
+    // Hot Module Replacement
+    hotOnly: true,
   },
 });
 
